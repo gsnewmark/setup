aac010499a1db35af916f82dd8c5b9da87b56824
Ivan Kryvoruchko <gildraug@gmail.com>
2013-09-08 11:59:39 +0300

    Replace variety with wallch.

--- setup.sh
+++ setup.sh
@@ -130,10 +130,8 @@ sudo add-apt-repository -y ppa:tiheum/equinox
 sudo apt-get update
 sudo apt-get -y install mediterraneannight-gtk-theme faenza-icon-theme faience-icon-theme
 
-# install variety wallpaper changer
-sudo add-apt-repository -y ppa:peterlevi/ppa
-sudo apt-get update
-sudo apt-get -y install variety
+# install wallch wallpaper changer
+sudo apt-get -y install wallch
 
 # install calibre
 sudo add-apt-repository -y ppa:n-muench/calibre
