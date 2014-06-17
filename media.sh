f9776ad021d32e6555f5838f1647cc69ad884da7
Ivan Kryvoruchko <gildraug@gmail.com>
2014-06-17 22:06:43 +0300

    Install mpdscribble.

--- media.sh
+++ media.sh
@@ -7,6 +7,9 @@ sudo pacman -S --noconfirm vlc deadbeef
 sudo pacman -S --noconfirm mpd ncmpcpp
 mkdir -p $HOME/.mpd/playlists
 touch $HOME/.mpd/{mpd.db,mpd.log,mpd.pid,mpdstate}
+yaourt -S --noconfirm mpdscribble
+mkdir $HOME/.mpdscribble
+cp /usr/share/mpdscribble/mpdscribble.conf.example $HOME/.mpdscribble/mpdscribble.conf
 
 # install Infobar plugin for deadbeef
 wget -O /tmp/deadbeef-infobar.zip http://sourceforge.net/projects/deadbeef/files/plugins/ddb_infobar-x86_64-1.4-171.zip/download
