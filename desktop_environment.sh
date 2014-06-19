2160f71f0744b7efbdcdd2ea1cfdf07631144c64
Ivan Kryvoruchko <gildraug@gmail.com>
2014-06-19 11:13:52 +0300

    Install xprop, xev and other xorg utils.

--- desktop_environment.sh
+++ desktop_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install DE + i3 WM
-yaourt -S --noconfirm xfce4 xfce4-notifyd xfce4-screenshooter gnome-keyring i3-wm python-i3-git i3blocks dmenu-xft-fuzzy xboomx quickswitch-i3 compton feh ttf-font-awesome skb gfvs
+yaourt -S --noconfirm xfce4 xfce4-notifyd xfce4-screenshooter gnome-keyring i3-wm python-i3-git i3blocks dmenu-xft-fuzzy xboomx quickswitch-i3 compton feh ttf-font-awesome skb gfvs xorg-utils
 
 # install terminal emulator
 sudo pacman -S --noconfirm gnome-terminal
