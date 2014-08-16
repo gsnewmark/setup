693c0a34242c5dd5ca10a970f5500fbe0b205d28
Ivan Kryvoruchko <gildraug@gmail.com>
2014-08-16 14:30:47 +0300

    Install silver searcher.

--- foundation.sh
+++ foundation.sh
@@ -17,7 +17,7 @@ chsh -s $(which zsh)
 sudo pacman -S python python2
 
 # install useful utils
-sudo pacman -S htop curl wget httpie xterm links xclip tmux lm_sensors rsync tree unzip
+sudo pacman -S htop curl wget httpie xterm links xclip tmux lm_sensors rsync tree unzip the_silver_searcher
 
 # install editors
 sudo pacman -S emacs vim
