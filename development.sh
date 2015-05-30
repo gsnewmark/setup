d457b93ff32d67f5c4855d63d568136a83e0984b
Ivan Kryvoruchko <gildraug@gmail.com>
2015-05-30 14:53:24 +0300

    Switch to haskell-core mirror repo

--- development.sh
+++ development.sh
@@ -27,7 +27,7 @@ sudo pacman -S ruby
 yaourt -S ruby-bundler
 
 # add Haskell-specific repo, install Haskell and some packages
-awk -v RS='' '/\[core\]/ { print; print "[haskell-core]"; print "Server = http://xsounds.org/~haskell/core/$arch"; next }1' /etc/pacman.conf > pacman.conf.tmp && sudo
+awk -v RS='' '/\[core\]/ { print; print "[haskell-core]"; print "Server = http://orbitalfox.com/haskell/core/$arch"; next }1' /etc/pacman.conf > pacman.conf.tmp && sudo
 mv pacman.conf.tmp /etc/pacman.conf
 sudo pacman-key -r 4209170B
 sudo pacman-key --lsign-key 4209170B
