fd57cc29ada48d4eafb4bdf63be80d99fc69b7a8
Ivan Kryvoruchko <gildraug@gmail.com>
2015-08-10 21:38:23 +0300

    Install M+ fonts

--- desktop_environment.sh
+++ desktop_environment.sh
@@ -5,7 +5,7 @@ yaourt -S i3-wm python-i3-git i3blocks quickswitch-i3 dunst dmenu2 \
           xfce4-settings xfce4-screenshooter gnome-keyring \
           compton feh ttf-font-awesome ttf-ms-fonts xorg-utils xdotool \
           udisks2 udiskie i3lock-wrapper bc polkit xkb-switch-git \
-          perl-file-mimeinfo adobe-source-code-pro-fonts
+          perl-file-mimeinfo adobe-source-code-pro-fonts ttf-mplus
 
 # install terminal emulator
 yaourt -S termite
