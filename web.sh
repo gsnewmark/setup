b9981ad2e9ac305b4048869ccc9f6ec0a346a20c
Ivan Kryvoruchko <gildraug@gmail.com>
2015-11-12 23:16:45 +0200

    Switch to the official Slack client

--- web.sh
+++ web.sh
@@ -30,8 +30,8 @@ sudo pacman -S transmission-gtk transmission-cli
 # install Skype + 32-bit GTK libs (for GTK themes support)
 sudo pacman -S skype lib32-gtk2
 
-# install unofficial Slack client
-yaourt -S scudcloud
+# install Slack client
+yaourt -S slack-desktop
 
 # install Java browser plugin
 sudo pacman -S icedtea-web
