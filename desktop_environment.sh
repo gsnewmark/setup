83c2d71063e07461fb0efd72ce80e987610235a6
Ivan Kryvoruchko <gildraug@gmail.com>
2016-08-27 15:11:21 +0300

    Switch to rofi

--- desktop_environment.sh
+++ desktop_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install i3 & various helpers & fonts & 32-bit GTK libs (for GTK themes support)
-yaourt -S i3-wm python-i3-git i3blocks quickswitch-i3 dunst dmenu2 \
+yaourt -S i3-wm python-i3-git i3blocks dunst rofi \
           xfce4-settings xfce4-screenshooter gnome-keyring \
           compton feh ttf-font-awesome ttf-ms-fonts xorg-utils xdotool \
           udisks2 udiskie i3lock-wrapper bc polkit xkb-switch-git \
